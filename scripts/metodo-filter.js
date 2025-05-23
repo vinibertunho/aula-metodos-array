d7ae2fb3e052de15ac079107a31ac498c2f1e89a
vinibertunho <vinicius.m.silva36@alno.senai.br>
2025-05-23 11:11:53 -0300

    feat: use o metodo filter

---
+++ scripts/metodo-filter.js
@@ -0,0 +1,5 @@
+let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz'];
+
+let playlistFiltrada = playlist.filter(musica => musica.length > 4);
+
+console.log("Playlist filtrada (músicas com mais de 4 letras):", playlistFiltrada);
