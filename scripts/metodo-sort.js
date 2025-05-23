faf4fdd34ef4edd3c0be1b3e2549fb760cf5d8af
vinibertunho <vinicius.m.silva36@alno.senai.br>
2025-05-23 11:21:54 -0300

    feat: use o metodo sort

--- scripts/metodo-sort.js
+++ scripts/metodo-sort.js
@@ -1,5 +1,13 @@
+// Método sort completo para ordenar o array playlist em ordem alfabética
+
 let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz'];
 
-let playlistOrdenada = playlist.sort();
+// Ordena em ordem alfabética (A-Z)
+playlist.sort();
 
-console.log("Playlist ordenada:", playlistOrdenada);
+console.log("Playlist ordenada (A-Z):", playlist);
+
+// Ordena em ordem reversa (Z-A)
+playlist.sort((a, b) => b.localeCompare(a));
+
+console.log("Playlist ordenada (Z-A):", playlist);
