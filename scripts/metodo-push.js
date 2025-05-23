71cb7e7f42083d1a8be6d9b4ed09ecd1cb267c63
vinibertunho <vinicius.m.silva36@alno.senai.br>
2025-05-23 08:57:39 -0300

    feat: cria um metodo push js dentro da pasta scripts

---
+++ scripts/metodo-push.js
@@ -0,0 +1,8 @@
+let playlist = ["funk", "hiphop", "pop"];
+console.log("antes do push");
+console.table(playlist);
+
+//adicionando um novo estilo
+playlist.push("trap");
+console.log("depois do push");
+console.table(playlist);
