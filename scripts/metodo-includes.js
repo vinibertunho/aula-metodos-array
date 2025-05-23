377dc5b90d534399c4fea220d7222aceb202e201
vinibertunho <vinicius.m.silva36@alno.senai.br>
2025-05-23 10:32:36 -0300

    feat: use o metodo includes

--- scripts/metodo-includes.js
+++ scripts/metodo-includes.js
@@ -1,5 +1,13 @@
-let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Opera', 'Hard-Rock', 'Ninar', 'Jazz'];
+let playlist = [
+  "Funk",
+  "Hip-Hop",
+  "Pop",
+  "Opera",
+  "Hard-Rock",
+  "Ninar",
+  "Jazz",
+];
 
-console.log("A playlist inclui 'Funk'?", playlist.includes('Funk')); // true
+console.log("A playlist inclui 'Funk'?", playlist.includes("Funk")); // true
 
-console.log("A playlist inclui 'Rock'?", playlist.includes('Country')); // false
+console.log("A playlist inclui 'Rock'?", playlist.includes("Country")); // false
