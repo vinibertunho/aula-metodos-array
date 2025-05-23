57d4090681b0eabd4ef3bdb03a7d8c37c3bc4832
vinibertunho <vinicius.m.silva36@alno.senai.br>
2025-05-23 09:54:48 -0300

    feat: usa o metodo pop

---
+++ scripts/metodo-shift.js
@@ -0,0 +1,10 @@
+let playlist = ["funk", "hiphop", "pop"];
+console.log("antes do push");
+console.table(playlist);
+
+// adicionando o primeiro estilo
+let estiloRemovido = playlist.shift()
+
+
+console.log("depois do shift")
+console.log("Estoli removido:", playlist)
