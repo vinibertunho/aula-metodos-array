180b56ae846c465209aae184004776aaa0fb0e55
vinibertunho <vinicius.m.silva36@alno.senai.br>
2025-05-23 10:42:39 -0300

    feat: use o metodo slice

---
+++ scripts/metodo-slice.js
@@ -0,0 +1,5 @@
+let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz', 'Reggae', 'Classical', 'Indie'];
+
+console.log("Parte da playlist (índices 2 a 5):", playlist.slice(2, 6)); // ['Pop', 'Rock', 'Jazz','Reggae']
+
+console.log("Parte da playlist (índices 0 a 3):", playlist.slice(0, 4)); // ['Funk', 'Hip-Hop', 'Pop','Rock']
